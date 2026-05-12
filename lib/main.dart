1537014e16bf41c8256fe19078504d4577f50d3e
Octowiaz <octavius0044@gmail.com>
2026-05-12 12:24:15 +0800

    Added Player Movement

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel/pixel.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
-  Flame.device.setLandscape();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
 
   Pixel game = Pixel();
-  runApp(GameWidget(game: kDebugMode ? Pixel(): game),
+  runApp(
+    GameWidget(game: kDebugMode ? Pixel(): game),
   );
 }
--- lib/pixel.dart
+++ lib/pixel.dart
@@ -2,28 +2,89 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/painting.dart';
+import 'package:pixel/actors/player.dart';
 import 'package:pixel/levels/level.dart';
 
-class Pixel extends FlameGame {
+class Pixel extends FlameGame with 
+  HasKeyboardHandlerComponents, DragCallbacks{
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
-
-  final world = Level(
-    levelName: 'Level_02',
-  );
+  Player player = Player(character: 'Virtual Guy');
+  late JoystickComponent joystick;
+  bool showJoystick = true;
 
   @override
   FutureOr<void> onLoad() async {
 
     await images.loadAllImages();
 
+    final world = Level(
+      player: player,
+      levelName: 'Level_02',
+  );
+
     cam = CameraComponent.withFixedResolution(
       world: world, width: 640, height: 360);
     cam.viewfinder.anchor = Anchor.topLeft;
+    cam.priority = 1;
 
     addAll([cam, world]);
+
+    if (showJoystick) {
+      addJoystick();
+    }
+
+    addJoystick();
+
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if(showJoystick) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+      priority: 2,
+    );
+
+    add(joystick);
+  }
+  
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
   }
 }
